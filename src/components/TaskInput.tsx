d75f36498a890197847c90e897f50a6fbbb6e278
Noah V <noah@noahvandenberg.com>
2023-04-16 16:33:29 -0700

    update TaskInput component

--- src/components/TaskInput.tsx
+++ src/components/TaskInput.tsx
@@ -1,7 +1,42 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
+import type { FormEvent } from 'react';
+import { mutate } from 'swr';
 
-function TaskInput() {
-  return <div>TaskInput</div>;
+function TaskInput({ search }: { search: string }) {
+  const [input, setInput] = useState('');
+
+  const handleCreateTask = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      await axios.post('http://localhost:3001/api/todos', {
+        value: input,
+        done: false,
+      });
+      setInput('');
+      mutate(['/api/todos', search]); // Include search parameter in the cache key
+    } catch (error) {
+      console.error('Error creating task:', error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleCreateTask} className='flex space-x-2'>
+      <input
+        type='text'
+        className='w-full px-3 py-1 border border-black rounded-md outline-none'
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button
+        type='submit'
+        className='rounded-md border border-black bg-[#cfe2f3] px-5 py-1'
+      >
+        Add
+      </button>
+    </form>
+  );
 }
 
 export default TaskInput;

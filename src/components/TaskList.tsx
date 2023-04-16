672277dad50f1fc088ab3fccc77c044ccdbe94f7
Noah V <noah@noahvandenberg.com>
2023-04-16 16:35:41 -0700

    update TaskList component

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,7 +1,64 @@
+import { fetcher } from '@/utils/apiClient';
 import React from 'react';
+import useSWR from 'swr';
+import TaskItem from '@/components/TaskItem';
 
-function TaskList() {
-  return <div>TaskList</div>;
+type Todo = {
+  id: number;
+  value: string;
+  done: boolean;
+  completedAt: string;
+};
+
+interface TaskListProps {
+  title: string;
+  search: string;
+}
+
+function TaskList({ title, search }: TaskListProps) {
+  const { data, error, isLoading, isValidating } = useSWR<{
+    todo: Todo[];
+    done: Todo[];
+  }>(['/api/todos', search], (keyArray: string[]) => {
+    const [, searchValue] = keyArray;
+    return fetcher(`/api/todos?search=${searchValue}`);
+  });
+
+  const loading = isLoading;
+  if (error)
+    return (
+      <div className='flex flex-col '>
+        <p className='pl-3'>{title}</p>
+        <div className='flex-1 my-1 border-b border-black w-100' />
+        <div className='flex flex-col space-y-2'>
+          <p>Error Loading Tasks</p>
+        </div>
+      </div>
+    );
+  if (loading)
+    return (
+      <div className='flex flex-col '>
+        <p className='pl-3'>{title}</p>
+        <div className='flex-1 my-1 border-b border-black w-100' />
+        <div className='flex flex-col space-y-2'>
+          <p>Loading......</p>
+        </div>
+      </div>
+    );
+
+  const items = title === 'Todo' ? data!.todo : data!.done;
+
+  return (
+    <div className='flex flex-col '>
+      <p className='pl-3'>{title}</p>
+      <div className='flex-1 my-1 border-b border-black w-100' />
+      <div className='flex flex-col space-y-2'>
+        {items.map((item) => (
+          <TaskItem key={item.id} item={item} search={search} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default TaskList;

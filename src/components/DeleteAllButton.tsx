862f94171fede293478e5785312498d8e3419c88
Noah V <noah@noahvandenberg.com>
2023-04-16 16:31:04 -0700

    update DeleteAllButton component

--- src/components/DeleteAllButton.tsx
+++ src/components/DeleteAllButton.tsx
@@ -1,7 +1,28 @@
+import axios from 'axios';
 import React from 'react';
+import type { MouseEvent } from 'react';
+import { mutate } from 'swr';
 
-function DeleteAllButton() {
-  return <div>DeleteAllButton</div>;
+function DeleteAllButton({ search }: { search: string }) {
+  const handleDeleteAll = async (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    if (window.confirm('Are you sure you want to delete all tasks?')) {
+      try {
+        await axios.delete('http://localhost:3001/api/todos');
+        mutate('/api/todos', search);
+      } catch (error) {
+        console.error('Error deleting tasks:', error);
+      }
+    }
+  };
+
+  return (
+    <button onClick={handleDeleteAll}>
+      <p className='self-center text-xs font-semibold text-blue-700 underline'>
+        Delete All Tasks
+      </p>
+    </button>
+  );
 }
 
 export default DeleteAllButton;

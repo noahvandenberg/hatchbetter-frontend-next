13981f4c9bf7b1d5808844856efb64ff2329da70
Noah V <noah@noahvandenberg.com>
2023-04-16 16:32:12 -0700

    update SearchInput component

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import type { ChangeEvent } from 'react';
 
-function SearchInput() {
-  return <div>SearchInput</div>;
+interface SearchInputProps {
+  value: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+function SearchInput({ value, onChange }: SearchInputProps) {
+  return (
+    <input
+      type='text'
+      placeholder='Search..'
+      className='self-end w-7/12 px-3 py-1 border border-black rounded-md outline-none'
+      value={value}
+      onChange={onChange}
+    />
+  );
 }
 
 export default SearchInput;

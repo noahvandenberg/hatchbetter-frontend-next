da30711bdf5008d711f84a3c631f4c544bbf085f
Noah V <noah@noahvandenberg.com>
2023-04-16 16:34:21 -0700

    create TaskItem component

---
+++ src/components/TaskItem.tsx
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import React from 'react';
+import { mutate } from 'swr';
+
+type Todo = {
+  id: number;
+  value: string;
+  done: boolean;
+  completedAt: string;
+};
+
+function TaskItem({ item, search }: { item: Todo; search: string }) {
+  const handleToggleDone = async () => {
+    try {
+      await axios.patch('http://localhost:3001/api/todos', {
+        id: item.id,
+        done: !item.done,
+      });
+      mutate(['/api/todos', search]); // Include search parameter in the cache key
+    } catch (error) {
+      console.error('Error updating task:', error);
+    }
+  };
+
+  return (
+    <div className='flex space-x-3'>
+      <input type='checkbox' checked={item.done} onChange={handleToggleDone} />
+      <p>{item.value}</p>
+    </div>
+  );
+}
+
+export default TaskItem;
